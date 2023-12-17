8880cf1c17cc34f855603aa12b832f8fb5a9bd30
Your Name <deckerwill7@gmail.com>
2023-12-17 10:17:37 -0600

    complete `get_kwargs()`

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import sys
 import os
 import subprocess
+import numpy as np
+from itertools import chain
 
 def check_dcm2niix():
     """Checks if dcm2niix is installed"""
@@ -11,18 +13,22 @@ def check_dcm2niix():
         syst = 'l' # linux
     elif sys.platform == 'darwin':
         syst = 'm' # mac OS
-        if os.path.exists('/opt/homebrew/bin/dcm2niix'):
-            dcm2niix_exist = True
     elif sys.platform == 'win31':
         syst = 'w'
-        if os.path.exists("C:\\Program Files\\dcm2niix\\dcm2niix.exe"):
-            dcm2niix_exist = True
+
+    if syst == 'm' and os.path.exists('/opt/homebrew/bin/dcm2niix'):
+        dcm2niix_exist = True
+    elif syst == 'l':
+        dcm2niix_exist = print(f'You are using Linux. Please ensure dcm2niix is properly installed.')
+    elif syst == 'w':
+        dcm2niix_exist = print(f'Windows functionality does not yet exist.')
 
     return(dcm2niix_exist)
+
 def check_gz(path):
-    """Checks if all individual files are uncompressed"""
+    """Checks if all individual files are uncompressed. If compressed, will uncompress in same directory."""
     # unzip file
-    if path.split[-1] == '.tgz':
+    if path.split('.')[-1] == '.tgz':
         subprocess.run(f'tar -kxvzf {path} -C {os.path.dirname(path)}', shell=True, check=True, text=True)
     else:
         pass
@@ -56,18 +62,56 @@ def get_kwargs(kwargs: dict):
         If you wish to convert a single file, provide a list as the arg for this parameter in which index [0] is bool
         and index[1] is the specific file you wish to convert
     """
+
+    if kwargs.get('fname_root') is None:
+         val = None
+    else:
+         val = kwargs.get('fname_root') + '%a'
+
+    if kwargs.get('single_file_path') is None:
+        val1 = None
+    else:
+        val1 = kwargs.get('single_file_path')
+
+    if kwargs.get("comp_level") is None:
+        val2 = None
+    else:
+        val2 = kwargs.get("comp_level")
+
     expect = {
-                "comp_level": 1,
-                "recomp": True,
-                "bidscar": True,
-                "anon": True,
-                "fname_root": "sub", 
-                "out": f"Users/{os.getlogin()}",
-                "single_file": False
+                "comp_level": val2,
+                "recomp": '-z',
+                "bidscar": '-b',
+                "anon": '-ba',
+                "fname_root":'-f', 
+                "single_file": '-s',
+                "single_file_path": val1,
+                "out": '-o'
               }
-    
-    for i in kwargs
+
+    if kwargs.get("bidscar") is True:
+        kwargs["bidscar"] = "y"
+
+    if kwargs.get("anon") is True:
+        kwargs["anon"] = "y"
+
+    # compare keys
+    got = list(set(expect.keys()).intersection(kwargs))
 
     out = []
-    for i in kwargs:
+    for i in got:
+        out.append(expect.get(i))
         out.append(kwargs.get(i))
+
+    for i in out:
+        if type(i) == bool:
+            out.remove(i)
+        else:
+            pass
+
+    out = ' '.join(map(str, out))
+    return(out)
+
+    
+
+        
